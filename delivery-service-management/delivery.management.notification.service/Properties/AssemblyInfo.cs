c02a12e1b897b272954ebf186ca3f10ae51ac1cd
Carlos Pons <karlospn@gmail.com>
2017-03-12 15:29:15 +0100

    Initial commit. Createad Web UI, registration service and notification service

---
+++ delivery-service-management/delivery.management.notification.service/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("delivery.management.notification.service")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9e685257-b29c-4cc7-a26a-04dee6f586a5")]
